a7f7fef6e3e771dbd320ad9ec84fcc91121760fe
TinaCheong <kanade11020512@gmail.com>
2021-12-05 19:46:48 +0800

    [Feature] Create UserDetailFragment and related layout

---
+++ app/src/main/java/com/tina/githubusers/userDetail/UserDetailFragment.kt
@@ -0,0 +1,25 @@
+package com.tina.githubusers.userDetail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.tina.githubusers.databinding.FragmentUserDetailBinding
+
+class UserDetailFragment: Fragment() {
+
+    private var _binding: FragmentUserDetailBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentUserDetailBinding.inflate(inflater, container, false)
+
+
+        return _binding!!.root
+    }
+}
